edaf58b0704c995918e04854a804b8fcd6e26eaf
Jip Irfandy <irfandyjip98@gmail.com>
2023-03-03 21:05:15 +0700

    feat: integrate `ComputerModule` with disk and cpu module

--- src/computer/computer.controller.ts
+++ src/computer/computer.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CpuService } from '../cpu/cpu.service';
+import { DiskService } from '../disk/disk.service';
 
 @Controller('computer')
-export class ComputerController {}
+export class ComputerController {
+  constructor(
+    private readonly cpuService: CpuService,
+    private readonly diskService: DiskService
+  ) {}
+
+  @Get()
+  run() {
+    return {
+      cpu: this.cpuService.compute(1,2),
+      disk: this.diskService.getData()
+    }
+  }
+}
